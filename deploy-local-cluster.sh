0929e151e03636579d92d1ce0df22a750ebd3038
agkanon <44835289+agkanon@users.noreply.github.com>
2022-02-08 12:33:32 +0600

    Update deploy-local-cluster.sh

--- deploy-local-cluster.sh
+++ deploy-local-cluster.sh
@@ -5,25 +5,25 @@ helm install ingress-nginx -n ingress-nginx --create-namespace ingress-nginx/ing
 
 helm repo add bitnami https://charts.bitnami.com/bitnami
 
-kubectl create ns hotel
+kubectl create ns keycloak
 
-helm install -n hotel keycloak-db bitnami/postgresql-ha
+helm install -n keycloak keycloak-db bitnami/postgresql-ha
 
 sleep 30
 
-kubectl apply -n hotel -f keycloak.yaml
+kubectl apply -n keycloak -f keycloak.yaml
 
 sleep 120
 
-openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:2048 -keyout auth-tls.key -out auth-tls.crt -subj "/CN=auth.localtest.me/O=hotel"
-kubectl create secret -n hotel tls auth-tls-secret --key auth-tls.key --cert auth-tls.crt
-kubectl apply -n hotel -f keycloak-ingress.yaml
+openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:2048 -keyout auth-tls.key -out auth-tls.crt -subj "/CN=auth.localtest.me/O=keycloak"
+kubectl create secret -n keycloak tls auth-tls-secret --key auth-tls.key --cert auth-tls.crt
+kubectl apply -n keycloak -f keycloak-ingress.yaml
 
 sleep 30
 
-kubectl get deployment -n hotel
-kubectl get service -n hotel
-kubectl get ingress -n hotel
+kubectl get deployment -n keycloak
+kubectl get service -n keycloak
+kubectl get ingress -n keycloak
 
 curl -k https://auth.localtest.me/auth/realms/master/protocol/openid-connect/certs
 
